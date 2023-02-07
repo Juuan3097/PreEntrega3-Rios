1949ad2150e9a52950e44078fc352359d4ea6ed9
Juan3097 <juan_k.97@hotmail.com>
2023-02-07 00:30:18 -0300

    Cuarto commit

--- script.js
+++ script.js
@@ -1,102 +1,92 @@
+const shopContent = document.getElementById("shopContent");
 
-function agregarCarrito(e){
-
-    let nombre_producto = this.nombre ;
-    let precio_producto = this.precio ;
-    let img_producto = this.img ;
-
-
-    let producto = {
-        nombre: nombre_producto,
-        precio: precio_producto,
-        img: img_producto,
-        cantidad:1
-    };
-
-    mostrar_carrito( producto );
-}
-
-// let boton = document.getElementById("botonCarrito")
-// boton.addEventListener("click", respuestaClick)
-// function agregarCarrito() {
-
-// }
 
 class Producto {
-    constructor(nombre, precio, img, cantidad) {
+    constructor(nombre, precio, img, cantidad, id) {
 
         this.nombre = nombre;
         this.precio = parseInt(precio);
         this.img = img;
         this.cantidad = parseInt(cantidad);
+        this.id = parseInt(id);
 
     }
 
 
 }
 
-let carrito = [];
+let productos = [];
 
-carrito.push(new Producto("Microondas", "2000", "img/microwave.jpg", "1"));
-carrito.push(new Producto("Horno", "3600", "img/oven.jpg", "1"));
-carrito.push(new Producto("Sofa", "1000", "img/sofa.jpg", "1"));
-carrito.push(new Producto("Mesa", "1800", "img/table.jpg", "1"));
-carrito.push(new Producto("Tostadora", "1200", "img/toaster.jpg", "1"));
-carrito.push(new Producto("Cacerola", "800", "img/pot.jpg", "1"));
-
-console.log(carrito);
-
-function mostrar_carrito(producto) {
-
-    let fila = document.createElement("tr");
-
-    fila.innerHTML = `<td><img src="${producto.img}"></td>
-                      <td>${producto.nombre}</td>
-                      <td>${producto.cantidad}</td>
-                      <td>${producto.precio}</td>
-                      <td><button class="btn btn-danger borrar_elemento">Borrar</button></td>`;
-
-    let tabla = document.getElementById("tbody");
-    tabla.append(fila);
-
-    // let btn_borrar = document.querySelectorAll(".borrar_elemento");
+productos.push(new Producto("Microondas", "2000", "img/microwave.jpg", "1", "1"));
+productos.push(new Producto("Horno", "3600", "img/oven.jpg", "1", "2"));
+productos.push(new Producto("Sofa", "1000", "img/sofa.jpg", "1", "3"));
+productos.push(new Producto("Mesa", "1800", "img/table.jpg", "1", "4"));
+productos.push(new Producto("Tostadora", "1200", "img/toaster.jpg", "1","5"));
+productos.push(new Producto("Cacerola", "800", "img/pot.jpg", "1","6"));
 
 
-    // for( let boton of btn_borrar){
+let carritoProductos = [];
 
-    //     boton.addEventListener("click" , borrar_producto);
-    // }
+productos.forEach((product) => {
+    let content = document.createElement("div");
+    content.className = "card";
+    content.innerHTML = `
+    <img src="${product.img}" alt="..." class="card-img-top" height="150" width="150">
+    <h3>${product.nombre}</h3>
+    <p>$${product.precio}</p>
+    `;
+
+    shopContent.append(content);
+
+    let comprar = document.createElement("button");
+    comprar.innerText = "Comprar";
+    comprar.className = "comprar";
 
 
+    content.append(comprar);
 
+    comprar.addEventListener("click", () => {
+        carritoProductos.push({
+            img: product.img,
+            nombre: product.nombre,
+            cantidad: product.cantidad,
+            precio: product.precio,
+            id: product.id
+        });
+
+            let tabla = document.getElementById("tbody");
+            let fila = document.createElement("tr");
+    
+            fila.innerHTML = `<td><img width=50 src="${product.img}"></td>
+            <td>${product.nombre}</td>
+            <td>${product.cantidad}</td>
+            <td>${product.precio}</td>
+            <td><button class="btn btn-danger borrar_elemento">Borrar</button></td>`;
+    
+            tabla.append(fila);
+   
+        console.log(carritoProductos);
+       
+    });
+});
+
+let btn_borrar = document.querySelectorAll(".borrar_elemento");
+
+    
+for( let boton of btn_borrar){
+
+    boton.addEventListener("click" , borrar_producto);
 }
 
 
-let btn_compra = document.querySelectorAll(".botonCarrito");
+function borrar_producto(id){
 
-for (let boton of btn_compra) {
+    let findId = carritoProductos.find((product) =>product.id === id);
+    console.log(findId);
 
-    boton.addEventListener("click", mostrar_carrito);
+    borrarItem = carritoProductos.filter((carritoId) =>{
+        return carritoId !== findId;
+    });
 
 }
 
-
-
-
-let btn_carrito = document.getElementById("mostrar_carrito");
-
-
-// btn_carrito.addEventListener("click" , function(){
-
-
-//     let carrito = document.getElementById("carrito");
-
-//     if( carrito.style.display != "none"){
-
-//         carrito.style.display = "none";
-//     }
-//     else{
-//         carrito.style.display = "block";
-//     }
-
-// })
